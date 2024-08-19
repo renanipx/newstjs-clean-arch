fef9abb1623127116c0fcb0b054bf2cfd8f0d0d3
renanipx <137443957+renanipx@users.noreply.github.com>
2024-08-19 19:55:40 -0300

    User entity validation tests with class validator

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity } from "@/shared/domain/entities/entity"
+import { Entity } from '@/shared/domain/entities/entity'
+import { UserValidatorFactory } from '../validators/user.validator'
 
 export type UserProps = {
   name: string
@@ -9,40 +10,48 @@ export type UserProps = {
 
 export class UserEntity extends Entity<UserProps> {
   constructor(public readonly props: UserProps, id?: string) {
+    UserEntity.validate(props)
     super(props, id)
-    this.props.createdAt = this.props.createdAt ?? new Date();
+    this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
-  update(value : string) : void {
-    this.name = value;
+  update(value: string): void {
+    UserEntity.validate({
+      ...this.props,
+      name: value,
+    })
+    this.name = value
   }
 
-  updatePassword(value : string) : void {
-    this.password = value;
+  updatePassword(value: string): void {
+    UserEntity.validate({
+      ...this.props,
+      password: value,
+    })
+    this.password = value
   }
 
   get name() {
     return this.props.name
   }
-
-  private set name(value:string){
-    this.props.name = value;
+  private set name(value: string) {
+    this.props.name = value
   }
-
   get email() {
     return this.props.email
   }
-
   get password() {
     return this.props.password
   }
-
-  private set password(value:string){
-    this.props.password = value;
+  private set password(value: string) {
+    this.props.password = value
   }
-
   get createdAt() {
     return this.props.createdAt
   }
 
+  static validate(props: UserProps) {
+    const validator = UserValidatorFactory.create()
+    validator.validate(props)
+  }
 }
